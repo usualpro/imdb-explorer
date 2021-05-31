38603e255603d66f42facca586481d1614792df1
Gary FELICITE-PRUDENT <gary.felicite-prudent@teladoc.com>
2021-05-31 18:11:37 +0200

    usage of swiper

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import { MediasList } from './components/MediasList';
 
 
-const App = () => <div className="container">
-
+const App = () => <div className="container d-grid gap-3 mt-3">
   <MediasList
     title="Top 250 des films"
     list={"Top250Movies"}
